9b646c85254150e992bc4b79e2ef038d93eb0dd7
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-04-12 17:50:01 -0300

    Example reading file in chunks and plot graph

--- com/datacamp/exercises/chunks/load_data_world_dev_ind.py
+++ com/datacamp/exercises/chunks/load_data_world_dev_ind.py
@@ -4,6 +4,9 @@ Created on 11 de abr de 2018
 @author: Edison Klafke Fillus
 '''
 
+import pandas as pd
+import matplotlib.pyplot as plt
+
 # Open a connection to the file
 with open('../../datasets/world_dev_ind.csv') as file:
 
@@ -80,4 +83,37 @@ with open('../../datasets/world_dev_ind.csv') as file:
 
 # Print            
 print(counts_dict)
+
+
+
+# Initialize reader object: urb_pop_reader
+urb_pop_reader = pd.read_csv('../../datasets/ind_pop.csv', chunksize=1000)
+
+# Initialize empty DataFrame: data
+data = pd.DataFrame()
+
+# Iterate over each DataFrame chunk
+for df_urb_pop in urb_pop_reader:
+
+    # Check out specific country: df_pop_ceb
+    df_pop_ceb = df_urb_pop[df_urb_pop['CountryCode'] == 'CEB']
+
+    # Zip DataFrame columns of interest: pops
+    pops = zip(df_pop_ceb['Total Population'],
+                df_pop_ceb['Urban population (% of total)'])
+
+    # Turn zip object into list: pops_list
+    pops_list = list(pops)
+
+    # Use list comprehension to create new DataFrame column 'Total Urban Population'
+    df_pop_ceb['Total Urban Population'] = [int(tup[0] * tup[1]) for tup in pops_list]
+    
+    # Append DataFrame chunk to data: data
+    data = data.append(df_pop_ceb)
+
+# Plot urban population data
+data.plot(kind='scatter', x='Year', y='Total Urban Population')
+plt.show()
+
+
 
