202ac9c639fc65031ecdf5d2478a47933d361365
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-04-22 22:30:29 -0300

    Include example for SQLITE data import

---
+++ datacamp/importdata/sqldatabasesdataimport.py
@@ -0,0 +1,29 @@
+# Import necessary module
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine('sqlite:///../../data/Chinook.sqlite')
+
+# Save the table names to a list: table_names
+table_names = engine.table_names()
+
+# Print the table names to the shell
+print(table_names)
+
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute("SELECT * FROM Album")
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+# Print the head of the DataFrame df
+print(df.head())
+
+
+# Execute query and store records in DataFrame: df
+df2 = pd.read_sql_query("SELECT * FROM Album", engine)
+
+# Confirm that both methods yield the same result: does df = df1 ?
+print(df.equals(df2))
