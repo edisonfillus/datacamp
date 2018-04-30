54d14a67f82db1569ad5b83a670294e2b6d87e3a
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-04-29 22:19:17 -0300

    Include examples of datatype converting

---
+++ datacamp/cleaningdata/datatypes.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+tips = pd.read_csv('./data/tips.csv')
+
+print(tips.info())
+
+# Convert the sex column to type 'category'
+tips.sex = tips.sex.astype('category')
+
+# Convert the smoker column to type 'category'
+tips.smoker = tips.smoker.astype('category')
+
+# Print the info of tips (see memory reduction)
+print(tips.info())
+
+# Convert 'total_bill' to a numeric dtype
+tips['total_bill'] = pd.to_numeric(tips['total_bill'], errors='coerce')
+
+# Convert 'tip' to a numeric dtype
+tips['tip'] = pd.to_numeric(tips['tip'], errors='coerce')
+
+# Print the info of tips
+print(tips.info())
