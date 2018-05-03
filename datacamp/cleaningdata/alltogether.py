0b4d7bd9c749da620c41c6d2ef3df61503f62e49
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-05-03 11:40:13 -0300

    Include example of melting data and converting fields

--- datacamp/cleaningdata/alltogether.py
+++ datacamp/cleaningdata/alltogether.py
@@ -1,22 +1,25 @@
+from re import sub
+
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-g1800s = pd.read_csv("./data/gapminder.csv")
+gapminder = pd.read_csv("./data/gapminder.csv", index_col=0)
 
-print(g1800s.head())
+print(gapminder.head())
 
-print(g1800s.info())
+print(gapminder.info())
 
-print(g1800s.describe())
+print(gapminder.describe())
 
-print(g1800s.shape)
+print(gapminder.shape)
 
-print(g1800s.columns)
+print(gapminder.columns)
 
 
 # Create the scatter plot
-g1800s.plot(kind='scatter', x='1800', y = '1899')
+gapminder.plot(kind='scatter', x='1800', y ='1899')
 
 # Specify axis labels
 plt.xlabel('Life Expectancy by Country in 1800')
@@ -42,10 +45,34 @@ def check_null_or_valid(row_data):
 
 
 # Check whether the first column is 'Life expectancy'
-assert g1800s.columns[-1] == 'Life expectancy'
+assert gapminder.columns[-1] == 'Life expectancy'
 
 # Check whether the values in the row are valid
-assert g1800s.iloc[:, :-2].apply(check_null_or_valid, axis=1).all().all()
+assert gapminder.iloc[:, :-2].apply(check_null_or_valid, axis=1).all().all()
+
+gapminder = gapminder.drop_duplicates(subset='Life expectancy')
 
 # Check that there is only one instance of each country (value_counts()[0] contains the most frequently occuring value
-assert g1800s['Life expectancy'].value_counts()[0] == 1
+assert gapminder['Life expectancy'].value_counts()[0] == 1
+
+# Melt gapminder: gapminder_melt
+gapminder_melt = pd.melt(gapminder, id_vars='Life expectancy')
+
+# Rename the columns
+gapminder_melt.columns = ['country','year','life_expectancy']
+
+# Print the head of gapminder_melt
+print(gapminder_melt.head())
+
+
+# Convert the year column to numeric
+gapminder_melt.year = pd.to_numeric(gapminder_melt.year)
+
+# Test if country is of type object
+assert gapminder_melt.country.dtypes == np.object
+
+# Test if year is of type int64
+assert gapminder_melt.year.dtypes == np.int64
+
+# Test if life_expectancy is of type float64
+assert gapminder_melt.life_expectancy.dtypes == np.float64
