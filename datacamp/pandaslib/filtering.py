f1ebd71e58a794883b91c472978b4990108f2b92
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-05-06 20:43:19 -0300

    Include example of filtering data by string

---
+++ datacamp/pandaslib/filtering.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df = pd.read_csv('./data/austin_airport_departure_data_2015_july.csv', index_col='Date', parse_dates=['Date'])
+
+
+# Strip extra whitespace from the column names: df.columns
+df.columns = df.columns.str.strip()
+
+# Extract data for which the destination airport is Dallas: dallas
+dallas = df['Destination Airport'].str.contains('DAL')
+
+# Compute the total number of Dallas departures each day: daily_departures
+daily_departures = dallas.resample('D').sum()
+
+# Generate the summary statistics for daily Dallas departures: stats
+stats = daily_departures.describe()
+
+print(dallas)
+
+print(stats)
