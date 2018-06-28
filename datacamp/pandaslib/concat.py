3f17cb1198f17f65fc3d00228fb80854452192cd
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-06-28 18:04:40 -0300

    Include examples of concat with multiindexed dataframes

--- datacamp/pandaslib/concat.py
+++ datacamp/pandaslib/concat.py
@@ -47,3 +47,35 @@ medals = pd.concat(medals, axis='columns')
 
 # Print medals
 print(medals)
+
+# USING MULTIINDEXED ROWS
+medals = []
+for medal in medal_types:
+    file_name = "./data/%s_top5.csv" % medal
+
+    # Read file_name into a DataFrame: medal_df
+    medal_df = pd.read_csv(file_name, index_col='Country')
+
+    # Append medal_df to medals
+    medals.append(medal_df)
+
+# Concatenate medals: medals
+medals = pd.concat(medals, keys=['bronze', 'silver', 'gold'])
+
+# Print medals in entirety
+print(medals)
+
+# Sort the entries of medals: medals_sorted
+medals_sorted = medals.sort_index(level=0)
+
+# Print the number of Bronze medals won by Germany
+print(medals_sorted.loc[('bronze','Germany')])
+
+# Print data about silver medals
+print(medals_sorted.loc['silver'])
+
+# Create alias for pd.IndexSlice: idx
+idx = pd.IndexSlice
+
+# Print all the data on medals won by the United Kingdom
+print(medals_sorted.loc[idx[:,'United Kingdom'],:])
