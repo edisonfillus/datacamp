c5f0ad0fbda5469458bf953101b4aa376537871e
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-07-02 22:29:50 -0300

    Include some examples of select with sqlalchemy

--- datacamp/sqlalchemy/basics.py
+++ datacamp/sqlalchemy/basics.py
@@ -23,3 +23,40 @@ print(census.columns.keys())
 
 # Print full table metadata
 print(repr(metadata.tables['census']))
+
+# Build select statement for census table: stmt
+stmt = 'SELECT * FROM census'
+
+# Execute the statement and fetch the results: results
+results = engine.execute(stmt).fetchall()
+
+# Print Results
+print(results)
+
+# Import select
+from sqlalchemy import select
+
+# Reflect census table via engine: census
+census = Table('census', metadata, autoload=True, autoload_with=engine)
+
+# Build select statement for census table: stmt
+stmt = select([census])
+
+# Print the emitted statement to see the SQL emitted
+print(stmt)
+
+results = engine.execute(stmt).fetchall()
+# Execute the statement and print the results
+print(results)
+
+# Get the first row of the results by using an index: first_row
+first_row = results[0]
+
+# Print the first row of the results
+print(first_row)
+
+# Print the first column of the first row by using an index
+print(first_row[0])
+
+# Print the 'state' column of the first row by using its name
+print(first_row['state'])
