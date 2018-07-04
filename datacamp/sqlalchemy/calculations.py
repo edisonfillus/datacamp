e4e6a9539ae21a3450ab9f6d34bb355264921235
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-07-04 11:19:17 -0300

    Include example of calculations with sqlalchemy

---
+++ datacamp/sqlalchemy/calculations.py
@@ -0,0 +1,50 @@
+# Import create_engine function
+from sqlalchemy import create_engine, MetaData, Table, select, desc, case, cast, func, Numeric
+
+# Create an engine to the census database
+engine = create_engine('mysql+pymysql://student:datacamp@courses.csrrinzqubik.us-east-1.rds.amazonaws.com:3306/census')
+
+# Print the table names
+print(engine.table_names())
+
+metadata = MetaData()
+
+# Reflect census table from the engine: census
+census = Table('census', metadata, autoload=True, autoload_with=engine)
+
+# Build query to return state names by population difference from 2008 to 2000: stmt
+stmt = select([census.columns.state, (census.columns.pop2008-census.columns.pop2000).label('pop_change')])
+
+# Append group by for the state: stmt
+stmt = stmt.group_by(census.columns.state)
+
+# Append order by for pop_change descendingly: stmt
+stmt = stmt.order_by(desc('pop_change'))
+
+# Return only 5 results: stmt
+stmt = stmt.limit(5)
+
+# Use connection to execute the statement and fetch all results
+results = engine.execute(stmt).fetchall()
+
+# Print the state and population change for each record
+for result in results:
+    print('{}:{}'.format(result.state, result.pop_change))
+
+# Build an expression to calculate female population in 2000
+female_pop2000 = func.sum(
+    case([
+        (census.columns.sex == 'F', census.columns.pop2000)
+    ], else_=0))
+
+# Cast an expression to calculate total population in 2000 to Float
+total_pop2000 = cast(func.sum(census.columns.pop2000), Numeric)
+
+# Build a query to calculate the percentage of females in 2000: stmt
+stmt = select([female_pop2000 / total_pop2000 * 100])
+
+# Execute the query and store the scalar result: percent_female
+percent_female = engine.execute(stmt).scalar()
+
+# Print the percentage
+print(percent_female)
