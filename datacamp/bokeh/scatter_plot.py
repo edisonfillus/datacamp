088c6889ae9527a587b511a33590b3cbfe7cf468
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-07-26 11:19:39 -0300

    Include examples of drawing plots with bokeh

--- datacamp/bokeh/scatter_plot.py
+++ datacamp/bokeh/scatter_plot.py
@@ -2,7 +2,7 @@
 from bokeh.plotting import figure
 
 # Import output_file and show from bokeh.io
-from bokeh.io import output_file, show
+from bokeh.io import output_file, show, reset_output
 
 female_literacy = [90.5, 50.8, 99.0, 88.8, 90.2, 40.0, 49.8, 48.8, 99.4, 99.0, 91.5, 93.9, 90.2, 99.0, 57.8, 22.8, 81.3,
                    77.2, 91.5, 56.1, 99.0, 99.0, 98.5, 89.2, 88.1, 96.6, 99.6, 96.9, 93.4, 66.3, 59.6, 97.7, 82.8, 99.3,
@@ -14,6 +14,14 @@ female_literacy = [90.5, 50.8, 99.0, 88.8, 90.2, 40.0, 49.8, 48.8, 99.4, 99.0, 9
                    53.0, 95.9, 97.8, 92.8, 99.7, 98.5, 49.5, 98.7, 99.4, 80.9, 93.1, 90.8, 97.8, 99.8, 87.7, 95.1, 95.4,
                    99.7, 83.5, 34.3, 36.5, 83.2, 99.8, 98.2, 90.4, 84.8, 85.6, 96.7, 89.4, 38.7, 89.1, 67.8, 90.7, 88.4,
                    79.3, 93.5, 93.3, 96.5, 99.0, 98.4, 79.5, 98.5, 83.3, 98.0, 99.1]
+
+female_literacy_africa = [48.8, 57.8, 22.8, 56.1, 88.1, 66.3, 59.6, 82.8, 63.9, 66.8, 44.1, 59.3, 40.1, 44.3, 65.3,
+                          67.8, 57.0, 21.6, 65.8, 15.1, 18.2, 61.0, 88.8, 33.0, 21.9, 71.0, 26.4, 66.1, 28.1, 59.9,
+                          53.7, 81.3, 28.9, 54.5, 41.1, 53.0, 49.5, 87.7, 95.1, 83.5, 34.3, 36.5, 83.2, 84.8, 85.6,
+                          89.1, 67.8, 79.3, 83.3]
+
+female_literacy_latinamerica = [90.2, 91.5, 93.4, 97.7, 84.6, 94.9, 98.7, 68.7, 81.7, 99.8, 88.3, 86.0, 83.5, 93.5,
+                                81.4, 77.9, 96.2, 92.8, 98.5, 90.8, 98.2, 88.4, 96.5, 98.0]
 
 fertility = [1.7690000000000001, 2.682, 2.077, 2.1319999999999997, 1.827, 3.872, 2.2880000000000003, 5.172999999999999,
              1.393, 1.262, 2.156, 3.0260000000000002, 2.033, 1.324, 2.8160000000000003, 5.211, 2.1, 1.781,
@@ -32,6 +40,16 @@ fertility = [1.7690000000000001, 2.682, 2.077, 2.1319999999999997, 1.827, 3.872,
              5.2829999999999995, 3.885, 0.966, 2.373, 2.6630000000000003, 1.251, 2.052, 3.3710000000000004, 2.093, 2.0,
              3.883, 3.852, 3.718, 1.732, 3.928]
 
+fertility_africa = [5.172999999999999, 2.8160000000000003, 5.211, 5.9079999999999995, 2.505, 5.52, 4.058, 4.859, 2.342,
+                    6.254, 2.334, 4.22, 4.967, 4.513999999999999, 4.62, 4.541, 5.6370000000000005, 5.841, 5.455, 7.069,
+                    5.405, 5.737, 3.363, 4.89, 6.081, 1.841, 5.329, 5.33, 5.377999999999999, 4.45, 4.166, 2.642, 5.165,
+                    4.5280000000000005, 4.697, 5.011, 4.388, 3.29, 3.264, 2.822, 4.968999999999999, 5.659, 3.24,
+                    1.7919999999999998, 3.45, 5.2829999999999995, 3.885, 2.6630000000000003, 3.718]
+
+fertility_latinamerica = [1.827, 2.156, 2.404, 2.2230000000000003, 2.53, 2.498, 1.926, 4.018, 2.513, 1.505, 2.612,
+                          3.3710000000000004, 3.19, 2.977, 2.295, 2.6830000000000003, 1.943, 2.516, 2.089, 2.362,
+                          1.6469999999999998, 2.373, 3.3710000000000004, 1.732]
+
 # Create the figure: p
 p = figure(x_axis_label='fertility (children per woman)', y_axis_label='female_literacy (% population)')
 
@@ -40,6 +58,40 @@ p.circle(fertility, female_literacy)
 
 # Call the output_file() function and specify the name of the file
 output_file('fert_lit.html')
+
+# Display the plot
+show(p)
+
+reset_output()
+
+# Create the figure: p
+p = figure(x_axis_label='fertility', y_axis_label='female_literacy (% population)')
+
+# Add a circle glyph to the figure p
+p.circle(fertility_latinamerica, female_literacy_latinamerica)
+
+# Add an x glyph to the figure p
+p.x(fertility_africa, female_literacy_africa)
+
+# Specify the name of the file
+output_file('fert_lit_separate.html')
+
+# Display the plot
+show(p)
+
+reset_output()
+
+# Create the figure: p
+p = figure(x_axis_label='fertility (children per woman)', y_axis_label='female_literacy (% population)')
+
+# Add a blue circle glyph to the figure p
+p.circle(fertility_latinamerica, female_literacy_latinamerica, color='blue', size=10, alpha=0.8)
+
+# Add a red circle glyph to the figure p
+p.circle(fertility_africa, female_literacy_africa, color='red', size=10, alpha=0.8)
+
+# Specify the name of the file
+output_file('fert_lit_separate_colors.html')
 
 # Display the plot
 show(p)
