89eba66c2e6cb701fa8fd71c91aebcc31dc5cfd5
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-08-13 11:14:33 -0300

    Include examples of simulations using bernoulli trial / binomials

---
+++ datacamp/exploratorydataanalysis/simulation.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def perform_bernoulli_trials(n, p):
+    """Perform n Bernoulli trials with success probability p
+    and return number of successes."""
+    # Initialize number of successes: n_success
+    n_success = 0
+
+    # Perform trials
+    for i in range(n):
+        # Choose random number between zero and one: random_number
+        random_number = np.random.random()
+
+        # If less than p, it's a success so add one to n_success
+        if random_number < p:
+            n_success += 1
+
+    return n_success
+
+
+def ecdf(data):
+    """Compute ECDF for a one-dimensional array of measurements."""
+    # Number of data points: n
+    n = len(data)
+
+    # x-data for the ECDF: x
+    x = np.sort(data)
+
+    # y-data for the ECDF: y
+    y = np.arange(1, n + 1) / n
+
+    return x, y
+
+
+# Seed random number generator
+np.random.seed(42)
+
+# Initialize the number of defaults: n_defaults
+n_defaults = np.empty(1000)
+
+# Compute the number of defaults
+for i in range(1000):
+    n_defaults[i] = perform_bernoulli_trials(100, 0.05)
+
+# Plot the histogram with default number of bins; label your axes
+_ = plt.hist(n_defaults, normed=True)
+_ = plt.xlabel('number of defaults out of 100 loans')
+_ = plt.ylabel('probability')
+
+# Show the plot
+plt.show()
+
+# Compute ECDF: x, y
+x, y = ecdf(n_defaults)
+
+# Plot the ECDF with labeled axes
+plt.plot(x, y, marker='.', linestyle='none')
+_ = plt.xlabel('Defaults')
+_ = plt.ylabel('ECDF')
+
+# Show the plot
+plt.show()
+
+# Compute the number of 100-loan simulations with 10 or more defaults: n_lose_money
+n_lose_money = np.sum(n_defaults >= 10)
+
+# Compute and print probability of losing money
+print('Probability of losing money =', n_lose_money / len(n_defaults))
+
+# Take 10,000 samples out of the binomial distribution: n_defaults
+n_defaults = np.random.binomial(n=100, p=0.05, size=10000)
+
+# Compute CDF: x, y
+x, y = ecdf(n_defaults)
+
+# Plot the CDF with axis labels
+plt.plot(x, y, marker='.', linestyle='none')
+_ = plt.xlabel('Defaults')
+_ = plt.ylabel('ECDF')
+
+# Show the plot
+plt.show()
