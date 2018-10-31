d614361f451f2916f6ddc53b8e851981ccb1a6e6
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-10-31 09:52:06 -0300

    Include example of logistic regression

---
+++ datacamp/learning/logistic_regression_tuning.py
@@ -0,0 +1,30 @@
+# Import the necessary modules
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn import datasets
+import numpy as np
+
+digits = datasets.load_breast_cancer()
+X = digits.data
+y = digits.target
+
+# Create training and test sets
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state=42)
+
+# Setup the hyperparameter grid
+c_space = np.logspace(-5, 8, 15)
+param_grid = {'C': c_space}
+
+# Instantiate a logistic regression classifier: logreg
+logreg = LogisticRegression()
+
+# Instantiate the GridSearchCV object: logreg_cv
+logreg_cv = GridSearchCV(logreg, param_grid, cv=5)
+
+# Fit it to the data
+logreg_cv.fit(X,y)
+
+# Print the tuned parameters and score
+print("Tuned Logistic Regression Parameters: {}".format(logreg_cv.best_params_))
+print("Best score is {}".format(logreg_cv.best_score_))
