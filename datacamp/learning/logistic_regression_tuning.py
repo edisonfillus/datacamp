c7f782d7bb3652efad4560bfb5e18d24215a1bc5
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-11-01 08:06:32 -0300

    train/test split

--- datacamp/learning/logistic_regression_tuning.py
+++ datacamp/learning/logistic_regression_tuning.py
@@ -10,11 +10,11 @@ X = digits.data
 y = digits.target
 
 # Create training and test sets
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state=42)
 
 # Setup the hyperparameter grid
 c_space = np.logspace(-5, 8, 15)
-param_grid = {'C': c_space}
+param_grid = {'C': c_space,'penalty': ['l1', 'l2']}
 
 # Instantiate a logistic regression classifier: logreg
 logreg = LogisticRegression()
@@ -22,8 +22,8 @@ logreg = LogisticRegression()
 # Instantiate the GridSearchCV object: logreg_cv
 logreg_cv = GridSearchCV(logreg, param_grid, cv=5)
 
-# Fit it to the data
-logreg_cv.fit(X,y)
+# Fit it to the training data
+logreg_cv.fit(X_train,y_train)
 
 # Print the tuned parameters and score
 print("Tuned Logistic Regression Parameters: {}".format(logreg_cv.best_params_))
